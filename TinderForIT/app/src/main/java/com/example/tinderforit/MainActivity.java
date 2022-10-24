55b11820f734f094747da2482319fdbad6f879d5
namvay0403 <76899642+namvay0403@users.noreply.github.com>
2022-10-24 22:09:39 +0700

    A18-22 <Màn hình ứng dụng khởi đầu>

---
+++ TinderForIT/app/src/main/java/com/example/tinderforit/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tinderforit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Choose_Login_And_Reg.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1500);
+    }
+}
