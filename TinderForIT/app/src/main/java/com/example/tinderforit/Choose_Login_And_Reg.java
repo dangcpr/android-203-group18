9c7c0fef28d09c5c35ccb3ac114299d384b896f0
Huy <xuanhuy.tqn.005@gmail.com>
2022-10-31 21:09:26 +0700

    A18-24 <Sign in - Log in with Facebook>

--- TinderForIT/app/src/main/java/com/example/tinderforit/Choose_Login_And_Reg.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/Choose_Login_And_Reg.java
@@ -3,57 +3,98 @@ package com.example.tinderforit;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Choose_Login_And_Reg extends Activity {
 
-    private Button mLogin, mRegister;
-
+    private FirebaseAuth mAuth;
+    private CallbackManager mCallbackManager;
+    LoginButton btnLoginFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_and_reg);
 
-        Button mLoginEmail = (Button) findViewById(R.id.login_with_email);
-        Button mLoginFacebook = (Button) findViewById(R.id.login_with_facebook);
-        Button mLoginPhone = (Button) findViewById(R.id.login_with_phone_number);
-        Button mLoginGoogle = (Button) findViewById(R.id.login_with_google);
+        mAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+        btnLoginFacebook = (LoginButton) findViewById(R.id.login_with_facebook);
+        btnLoginFacebook.setReadPermissions("email", "public_profile");
 
-        mLoginEmail.setOnClickListener(new View.OnClickListener() {
+        btnLoginFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Choose_Login_And_Reg.this, com.example.tinderforit.LoginEmailActivity.class);
-                startActivity(i);
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(Choose_Login_And_Reg.this,"Thanh cong", Toast.LENGTH_LONG).show();
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
             }
         });
 
-        mLoginFacebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Choose_Login_And_Reg.this, com.example.tinderforit.LoginFacebookActivity.class);
-                startActivity(i);
-            }
-        });
+    }
 
-        mLoginPhone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Choose_Login_And_Reg.this, com.example.tinderforit.LoginPhoneActivity.class);
-                startActivity(i);
-            }
-        });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
-        mLoginGoogle.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Choose_Login_And_Reg.this, com.example.tinderforit.LoginGoogleActivity.class);
-                startActivity(i);
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    Toast.makeText(Choose_Login_And_Reg.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
             }
         });
+    }
+
+    private void updateUI(FirebaseUser user) {
 
     }
 }
--- TinderForIT/app/src/main/java/com/example/tinderforit/LoginFacebookActivity.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/LoginFacebookActivity.java
@@ -3,13 +3,26 @@ package com.example.tinderforit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginFacebookActivity extends Activity {
+
+    Button btnBack = (Button) findViewById(R.id.Back);;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_facebook);
+
+        /*btnBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                Intent i = new Intent(LoginFacebookActivity.this, Choose_Login_And_Reg.class);
+                startActivity(i);
+            }
+        });*/
     }
 }
