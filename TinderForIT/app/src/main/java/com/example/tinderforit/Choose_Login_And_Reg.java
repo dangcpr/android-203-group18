d705d2a9cb2a2afb98e92120a73d12e61c608ee0
Huy <xuanhuy.tqn.005@gmail.com>
2022-11-01 17:32:17 +0700

    A18-24 <Sign in - Log in with Facebook>

--- TinderForIT/app/src/main/java/com/example/tinderforit/Choose_Login_And_Reg.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/Choose_Login_And_Reg.java
@@ -19,8 +19,10 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.internal.BaseGmsClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -28,41 +30,50 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
+import java.util.Arrays;
 
 public class Choose_Login_And_Reg extends Activity {
 
     private FirebaseAuth mAuth;
     private CallbackManager mCallbackManager;
-    LoginButton btnLoginFacebook;
+    Button btnLoginFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_and_reg);
 
+        btnLoginFacebook = (Button) findViewById(R.id.login_with_facebook);
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
         mAuth = FirebaseAuth.getInstance();
         mCallbackManager = CallbackManager.Factory.create();
-        btnLoginFacebook = (LoginButton) findViewById(R.id.login_with_facebook);
-        btnLoginFacebook.setReadPermissions("email", "public_profile");
 
-        btnLoginFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+        // Kết nối với facebook
+        LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            // Kết nối thành công
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(Choose_Login_And_Reg.this,"Thanh cong", Toast.LENGTH_LONG).show();
                 handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
+            // Không cho phép kết nối
             @Override
-            public void onCancel() {
-
-            }
-
+            public void onCancel() {}
             @Override
-            public void onError(FacebookException error) {
-
-            }
+            public void onError(FacebookException error) {}
         });
 
+        btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(Choose_Login_And_Reg.this,
+                        Arrays.asList("email", "public_profile"));
+            }
+        });
     }
 
     @Override
@@ -71,23 +82,30 @@ public class Choose_Login_And_Reg extends Activity {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
     private void handleFacebookAccessToken(AccessToken token) {
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
         mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+
                 if (task.isSuccessful()) {
                     FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
+                    boolean _isNewUser = task.getResult().getAdditionalUserInfo().isNewUser();
+                    if(_isNewUser){
+                        Toast.makeText(Choose_Login_And_Reg.this,"Đăng kí",Toast.LENGTH_SHORT).show();
+                        // Activity Đăng kí
+                        // Tạm thời giả sử đăng kí xong, vào main luôn // Xài activity giả là LoginFbActivity, Activity này không cần dùng đến
+                        Intent myIntent = new Intent(Choose_Login_And_Reg.this,LoginFacebookActivity.class);
+                        startActivity(myIntent);
+                    }else{
+                        Toast.makeText(Choose_Login_And_Reg.this,"Đăng nhập",Toast.LENGTH_SHORT).show();
+                        // Activity Main
+                        Intent myIntent = new Intent(Choose_Login_And_Reg.this,LoginFacebookActivity.class);
+                        startActivity(myIntent);
+                    }
                 } else {
-                    Toast.makeText(Choose_Login_And_Reg.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Choose_Login_And_Reg.this, "Error", Toast.LENGTH_SHORT).show();
                     updateUI(null);
                 }
             }
--- TinderForIT/app/src/main/java/com/example/tinderforit/LoginFacebookActivity.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/LoginFacebookActivity.java
@@ -7,22 +7,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginFacebookActivity extends Activity {
 
-    Button btnBack = (Button) findViewById(R.id.Back);;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_facebook);
 
-        /*btnBack.setOnClickListener(new View.OnClickListener(){
+        btnBack = (Button) findViewById(R.id.Back);
+
+        btnBack.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v)
             {
+                Toast.makeText(LoginFacebookActivity.this,"Đăng Xuất", Toast.LENGTH_SHORT).show();
+                // Đăng xuất trên firebase
+                FirebaseAuth.getInstance().signOut();
                 Intent i = new Intent(LoginFacebookActivity.this, Choose_Login_And_Reg.class);
                 startActivity(i);
             }
-        });*/
+        });
     }
 }
