347b6ea408f475c00081bb4b094f42ba1bbb6b88
namvay0403 <76899642+namvay0403@users.noreply.github.com>
2022-11-07 15:48:33 +0700

    comment code login with FB and google because of having some conflicts

--- TinderForIT/app/src/main/java/com/example/tinderforit/MainActivity.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/MainActivity.java
@@ -30,14 +30,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //connect firebase and google login request
-        mAuth=FirebaseAuth.getInstance();
-        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        gsc= GoogleSignIn.getClient(this,gso);
-        /*
+//        //connect firebase and google login request
+//        mAuth=FirebaseAuth.getInstance();
+//        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken(getString(R.string.default_web_client_id))
+//                .requestEmail()
+//                .build();
+//        gsc= GoogleSignIn.getClient(this,gso);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -46,40 +46,40 @@ public class MainActivity extends Activity {
                 finish();
             }
         }, 1500);
-         */
+
         //display user info
 
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-        if (acct != null) {
-            String personName = acct.getDisplayName();
-            String personGivenName = acct.getGivenName();
-            String personFamilyName = acct.getFamilyName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
-            Uri personPhoto = acct.getPhotoUrl();
-            //show email address
-            Toast.makeText(MainActivity.this,personEmail,Toast.LENGTH_SHORT).show();
-        }
-
-        btnLogOut=(Button) findViewById(R.id.btnLogout);
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LogOut();
-            }
-        });
-    }
-
-    private void LogOut(){
-        gsc.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        mAuth.signOut();
-                        //finish();
-                        Intent myIntent=new Intent(MainActivity.this,Choose_Login_And_Reg.class);
-                        startActivity(myIntent);
-                    }
-                });
+//        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+//        if (acct != null) {
+//            String personName = acct.getDisplayName();
+//            String personGivenName = acct.getGivenName();
+//            String personFamilyName = acct.getFamilyName();
+//            String personEmail = acct.getEmail();
+//            String personId = acct.getId();
+//            Uri personPhoto = acct.getPhotoUrl();
+//            //show email address
+//            Toast.makeText(MainActivity.this,personEmail,Toast.LENGTH_SHORT).show();
+//        }
+//
+//        btnLogOut=(Button) findViewById(R.id.btnLogout);
+//        btnLogOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                LogOut();
+//            }
+//        });
+//    }
+//
+//    private void LogOut(){
+//        gsc.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        mAuth.signOut();
+//                        //finish();
+//                        Intent myIntent=new Intent(MainActivity.this,Choose_Login_And_Reg.class);
+//                        startActivity(myIntent);
+//                    }
+//                });
     }
 }
