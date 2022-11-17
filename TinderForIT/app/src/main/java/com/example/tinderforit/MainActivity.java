5d5b8ea7a03a87a87c32efe7ed7875c7328ed213
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2022-11-17 23:22:41 +0700

    A18-40 <Basic setting profile + Realtime Database

--- TinderForIT/app/src/main/java/com/example/tinderforit/MainActivity.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/MainActivity.java
@@ -77,4 +77,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    
 }
--- TinderForIT/app/src/main/java/com/example/tinderforit/ProfileFragment.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.tinderforit;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +34,34 @@ public class ProfileFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    ImageView imgGallery;
+    Button btnGallery;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int GALlERY_REG_CODE = 1000 ;
+
+    private Button btnChooseimg;
+
+    private String userid;
+    private String email;
+
+    private TextInputLayout LfName;
+    private TextInputEditText TfName;
+    private String firstName;
+
+    private TextInputLayout LlName;
+    private TextInputEditText TlName;
+    private String lastName;
+
+    DatePicker picker;
+    private TextInputEditText TDOB;
+    private TextInputLayout LDOB;
+    private String dateOfBirth;
+
+    private Button btnSend;
+
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -49,16 +88,126 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnSend = (Button) v.findViewById(R.id.sendinfo);
+
+        TfName = (TextInputEditText) v.findViewById(R.id.textFirstName);
+        LfName = (TextInputLayout) v.findViewById(R.id.layouttextFirstName);
+
+        TlName = (TextInputEditText) v.findViewById(R.id.textLastName);
+        LlName = (TextInputLayout)v.findViewById(R.id.layouttextLastName);
+
+        LDOB = (TextInputLayout) v.findViewById(R.id.layouttextDOB);
+        TDOB = (TextInputEditText) v.findViewById(R.id.textDOB);
+
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        LDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our edit text.
+                                TDOB.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
+            }
+        });
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if (TfName.getText().toString().isEmpty()) {
+                        TfName.setError("First name is missing");
+                        return;
+                    }
+                    if (TlName.getText().toString().isEmpty()) {
+                        TfName.setError("Last name is missing");
+                        return;
+                    }
+                    if (TDOB.getText().toString().isEmpty()) {
+                        TDOB.setError("Last name is missing");
+                        return;
+                    }
+
+                    firstName = String.valueOf(LfName.getEditText().getText());
+                    lastName = String.valueOf(LlName.getEditText().getText());
+                    dateOfBirth = String.valueOf(LDOB.getEditText().getText());
+                    email = user.getEmail();
+                    userid = user.getUid();
+
+                    UserProfile userProfile = new UserProfile(email, firstName, lastName, dateOfBirth);
+
+                    mDatabase.child("UserProfile").child(userid).setValue(userProfile);
+                    Toast.makeText(getActivity(), "Success", Toast.LENGTH_LONG).show();
+                } catch (Exception error1) {
+                    Toast.makeText(getActivity(), "Failure" + error1.getMessage(), Toast.LENGTH_LONG).show();
+                    error1.printStackTrace();
+                }
+            }
+        });
+        return v;
+    }
+}
+
+@IgnoreExtraProperties
+class UserProfile {
+
+    public String userid;
+    public String email;
+    public String firstName;
+    public String lastName;
+    public String dateOfBirth;
+
+    public UserProfile() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public UserProfile(String email, String firstName, String lastName, String dateOfBirth) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
     }
 }
