3c86686ae25792b1fc3d93124fc928698ae85fc4
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2022-11-22 00:14:49 +0700

    A18-40 <UI Home>

--- TinderForIT/app/src/main/java/com/example/tinderforit/HomeFragment.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/HomeFragment.java
@@ -59,7 +59,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
         return v;
     }
 }
