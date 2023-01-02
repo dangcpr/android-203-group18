87c1d48822df422ce9eacfbe6ff9e03c07e2f07a
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2023-01-02 22:50:32 +0700

    Change code

--- TinderForIT/app/src/main/java/com/example/tinderforit/ChatFragment.java
+++ TinderForIT/app/src/main/java/com/example/tinderforit/ChatFragment.java
@@ -42,8 +42,7 @@ public class ChatFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {     super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
